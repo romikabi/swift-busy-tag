2b104da67ccc4fd3cf05382fc0de0aa882e18045
Rome Abuziarov <abuzyarovroman@gmail.com>
2025-05-02 16:29:05 +0200

    add SwiftSerial dependency

--- Package.swift
+++ Package.swift
@@ -1,24 +1,23 @@
 // swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "swift-busy-tag",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "swift-busy-tag",
-            targets: ["swift-busy-tag"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "swift-busy-tag"),
-        .testTarget(
-            name: "swift-busy-tagTests",
-            dependencies: ["swift-busy-tag"]
-        ),
-    ]
+  name: "swift-busy-tag",
+  products: [
+    .library(
+      name: "swift-busy-tag",
+      targets: ["swift-busy-tag"]
+    ),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/mredig/SwiftSerial.git", from: "0.1.5"),
+  ],
+  targets: [
+    .target(name: "swift-busy-tag"),
+    .testTarget(
+      name: "swift-busy-tag-tests",
+      dependencies: ["swift-busy-tag"]
+    ),
+  ]
 )
